3f0be3eca6c3b00824283a62fad3e5936eccaa5c
testtest0192ow-rgb <testtest0192ow@gmail.com>
2026-07-07 16:36:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,14 @@ async def setup(ctx):
     button = discord.ui.Button(label="ПРОЙТИ ВЕРИФИКАЦИЮ", style=discord.ButtonStyle.primary, emoji="🛡️")
 
     async def button_callback(interaction: discord.Interaction):
-        role = discord.utils.get(interaction.guild.roles, id=1523339212517019729)
-        if role:
-            await interaction.user.add_roles(role)
+        # ID ролей
+        v_role = discord.utils.get(interaction.guild.roles, id=1523339212517019729) # Верифицированный
+        u_role = discord.utils.get(interaction.guild.roles, id=1523796444312837621) # Анвериф
+        
+        if v_role:
+            await interaction.user.add_roles(v_role) # Даем роль
+            if u_role:
+                await interaction.user.remove_roles(u_role) # Забираем анвериф
             await interaction.response.send_message("Доступ открыт!", ephemeral=True)
         else:
             await interaction.response.send_message("Ошибка: роль не найдена.", ephemeral=True)
